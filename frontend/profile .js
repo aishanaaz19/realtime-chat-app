d24c67e8cb7c0f424e6cee291ebe5ee88e5a240d
mca28kajal <kajalkumari6201834@gmil.com>
2025-02-13 10:57:53 +0530

    update welcome

---
+++ frontend/profile .js
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <link rel="icon" type="image/svg+xml" href="/vite.svg" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Vite + React</title>
+  </head>
+  <body>
+    <div id="root">
+      <h1>this is a heading</h1>
+      <h2>another heading</h2>
+      <h3>check app</h3>
+
+    </div>
+    <script type="module" src="/src/main.jsx"></script>
+  </body>
+</html>
