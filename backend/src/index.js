dad88f3a286c88cad57e8b04e247714fe5d948c5
ayeshakarim19 <84725003+ayeshakarim19@users.noreply.github.com>
2025-02-17 23:10:34 +0530

    Updated routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,19 @@
 import express from "express";
-import authRoutes from "../routes/auth.route.js";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
+import { connectDB} from "./lib/db.js";
+import authRoutes from "./routes/auth.route.js";
+
+dotenv.config();
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT;
 
-app.use("/api/auth", authRoutes)
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-    
-})
+    console.log("Server is running on PORT:" + PORT);
+    connectDB();
+});
