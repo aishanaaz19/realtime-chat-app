b8e36edd7472c388c9f9a1d48e9a59a4465756a4
ayeshakarim19 <84725003+ayeshakarim19@users.noreply.github.com>
2025-06-20 02:44:17 +0530

    Fixed Issues

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import path from "path";
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
 
 import { connectDB} from "./lib/db.js";
 import userRoutes from './routes/user.route.js'
@@ -13,7 +14,8 @@ import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 const PORT = process.env.PORT;
-
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 app.use(express.json({ limit: '30mb' })); 
 app.use(express.urlencoded({ limit: '30mb', extended: true })); 
@@ -29,9 +31,9 @@ app.use("/api/messages", messageRoutes);
 app.use("/api/chatbot", chatbotRoutes);
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    app.use(express.static(join(__dirname, "../frontend/dist")));
     app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+        res.sendFile(join(__dirname, "../frontend/dist/index.html"));
     });
 }
 
